7c1f9283d01ee67be7a135bb74367950d5ccec52
MeeraPanchal74 <meerapanchal1494@gmail.com>
2023-09-20 15:33:28 +0530

    Initial commit for Login page
    Co-authored by: Khushbu 202101512@daiict.ac.in

---
+++ src/Login/Login.js
@@ -0,0 +1,125 @@
+import React from 'react';
+import './Login.css';
+import Phone from './Phone.png';
+import document from './document.png';
+import Logo from './Logo.png';
+import {Link} from  'react-router-dom';
+import {useState} from 'react';
+
+
+const Login = () => {
+    const[regi, setRegi] = useState();
+    const handleEventChange= (evnt)=>{
+        setRegi(evnt.target.value);
+    }
+
+    return (
+        <div class='log'>
+            <div id='nav'>
+
+                <div id='leftnav'>
+                <Link to=''>
+                <img src={document} alt="legal section" id='legal' />
+                    </Link>
+                    <Link to=''>
+                    <img src={Phone} alt="contact us" id='contact' />
+                    </Link>
+                </div>
+
+                <div id='rightnav'>
+                    <button id='snup'>
+                        <Link to='/Register'>
+                            Sign up
+                        </Link>
+
+                    </button>
+                </div>
+
+            </div>
+
+            <div id='bdy'>
+                <div id='form'>
+
+                    <h1>Sign in</h1>
+
+                    <div id='check'>
+
+                        <div id='radio1'>
+                            <label>
+                                <input type="radio" name='regi' value='Individual' onChange={handleEventChange}/>
+                                Individual
+                            </label>
+                        </div>
+
+                        <div id='radio2'>
+                            <label>
+                                <input type="radio" name='regi' value='Organisation' onChange={handleEventChange}/>
+                                Organisation
+                            </label>
+                        </div>
+                    </div>
+
+
+                    <div>
+                        <p className='adjust'>
+                            Email or phone number
+                        </p>
+                        <input type='text' className='inbox' id='email'>
+                        </input>
+
+                    </div>
+
+                    <div>
+                        <p className='adjust'>
+                            Password
+                        </p>
+                        <div>
+                                <input type='password' className='inbox' id='pswd'/>
+                        </div>
+                    </div>
+
+                    <div id='snBtn'>
+                        <button id='signInbtn' >
+                            <Link to=''>
+                            Sign in
+                            </Link>
+                        </button>
+                    </div>
+
+                    <div id='footer'>
+
+                        <div id='remember'>
+                            <label>
+                                <input type='checkbox' value='Remember me' />
+                                Remember me
+                            </label>
+                        </div>
+                      
+                        <div id='help'>
+                        <Link to=''>
+                                Need help?
+                                </Link>
+                        </div>
+                       
+                        
+                    </div>
+
+                    <div id='lastLine'>
+                        Don't have an account?
+
+                        <Link to="/Register">
+                            Sign up
+                        </Link>
+
+                    </div>
+                 </div>
+                <div id='lg'>
+                    <img src={Logo} alt="startApp logo" id='SAlogo' />
+                </div>
+
+            </div>
+         </div>
+    )
+}
+
+export default Login;
--- src/Root.jsx
+++ src/Root.jsx
@@ -6,13 +6,19 @@ import Header from './Header';
 import IndividualList from './pages/IndividualList';
 import OrganizationList from './pages/OrganizationList';
 import JobSeekerList from './pages/JobSeekerList';
+import Login from './Login/Login';
+import Register from './Login/Register';
+
 function Root() {
     return (
         <>
             <HashRouter>
-                <Header />
+                {/* <Header /> */}
                 <Routes>
-                    <Route path="/" element={<h1> Home </h1>} />
+                <Route exact path="/"  element={<Login/>}/>    
+                    <Route exact path='/Register' element= { <Register/> }/>
+                    <Route exact path="/Login" element ={<Login/>}/>
+                    {/* <Route path="/" element={<h1> Home </h1>} /> */}
                     <Route path="/individuals" element={<IndividualList />} />
                     <Route path="/organizations" element={<OrganizationList />} />
                     <Route path="/jobseekers" element={<JobSeekerList />} />
