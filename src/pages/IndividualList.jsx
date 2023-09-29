f0c2a756987f38f98097c7240d4e4416bd23f7c5
sohamviradiya <sohamviradiya2003@gmail.com>
2023-09-29 21:40:03 +0530

    set up react query for individuals and organizations list

--- src/pages/IndividualList.jsx
+++ src/pages/IndividualList.jsx
@@ -1,34 +1,88 @@
-import { useEffect, useState } from "react";
-import { fetchIndividuals } from "../database/individual";
+import { useEffect, useMemo, useState } from "react";
 import IndividualCard from "../components/IndividualCard";
+import IndividualData from "../database/individual";
+import { useDeferredValue } from "react";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { Box, Button, CircularProgress, Container, Grid, ListItem, Paper, Skeleton, TextField, Typography } from "@mui/material";
+
+const { asyncFetchIndividuals } = IndividualData;
 
 export default function IndividualList() {
-    const [individuals, setIndividuals] = useState([]);
-    const [query, setQuery] = useState("");
-    const [page, setPage] = useState(1);
-    const [isLoading, setIsLoading] = useState(false);
+    const [query, setQuery] = useState(" ");
+    const deferredQuery = useDeferredValue(query, { timeoutMs: 1000 });
     useEffect(() => {
-        if (page === -1) return;
-        setIsLoading(true);
-        fetchIndividuals({ query, page }).then((data) => {
-            setIndividuals(data);
-            setIsLoading(false);
-        });
-    }, [query, page]);
+        setQuery("");
+    }, []);
+
+    const { data, fetchNextPage, hasNextPage, isFetching, isFetchingNextPage, isLoading, isError, error } = useInfiniteQuery({
+        queryKey: ["organizations", deferredQuery],
+        queryFn: ({ pageParam }) => asyncFetchIndividuals({ query: deferredQuery, page: pageParam + 1 || 1 }),
+        getNextPageParam: (lastPage, pages) => {
+            if (lastPage.length < 10) {
+                return null;
+            }
+            return pages.length;
+        }
+    });
+
+    const individuals = useMemo(() => {
+        if (data) {
+            return data.pages.flatMap((page) => page);
+        }
+        return [];
+    }, [data]);
 
     return (
-        <>
-            <h1>Individuals</h1>
-            <input value={query} onChange={(e) => setQuery(e.target.value)} />
-            {isLoading && <h2> Loading...</h2>}
-            <ul>
-                {individuals.map((individual) => (
-                    <li key={individual.username}>
-                        <IndividualCard individual={individual} isLoading={isLoading} />
-                    </li>
+        <Container maxWidth="lg" sx={{ display: "flex", flexDirection: "column", alignItems: "center", gap: "2rem" }}>
+        <Typography variant="h1">Individuals</Typography>
+        <Box border={1} p={2} borderRadius={2} sx={{ display: "flex", flexDirection: "row", alignItems: "center", gap: "1rem" }}>
+            <TextField
+                label="Search"
+                variant="outlined"
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
+                focused
+            />
+            {isFetching && <CircularProgress color="secondary" />}
+        </Box>
+        <Paper elevation={3} style={{ padding: '1rem', minWidth: "80vw" }}>
+            {isLoading && !data && <Skeleton variant="rectangular" height={600} />}
+            {isError && (
+                <Typography variant="h2" color="error">
+                    Error: {error.message}
+                </Typography>
+            )}
+            <Grid container spacing={1}>
+                {individuals.map((id) => (
+                    <Grid item key={id} xs={12} sm={6}>
+                        <ListItem>
+                            <IndividualCard id={id} isLoadingData={isLoading} />
+                        </ListItem>
+                    </Grid>
                 ))}
-            </ul>
-            <button onClick={() => setPage((page) => (page + 1))} disabled={page === -1} > Next Page </button>
-            <button onClick={() => setPage((page) => (page - 1))} disabled={page === 1} > Previous Page </button>
-        </>);
+            </Grid>
+            <Button
+                onClick={() => fetchNextPage()}
+                disabled={!hasNextPage || isFetchingNextPage}
+                variant="contained"
+                color="primary"
+                fullWidth={true}
+                sx={{ marginTop: "1rem" }}
+                onMouseOver={() => {
+                    if (!hasNextPage || isFetchingNextPage)
+                        return;
+                    fetchNextPage();
+                }}
+            >
+                {isFetchingNextPage ?
+                    <Typography variant="h5" color="info.main">
+                        Loading more...
+                    </Typography>
+                    : hasNextPage ? <Typography variant="h5">Load More</Typography>
+                        : <Typography variant="h5">Nothing more to load</Typography>
+                }
+            </Button>
+        </Paper>
+    </Container>
+    );
 }
--- src/pages/OrganizationList.jsx
+++ src/pages/OrganizationList.jsx
@@ -1,36 +1,86 @@
-import { useEffect, useState } from "react";
-import { fetchOrganizations } from "../database/organization";
+import { useDeferredValue, useEffect, useMemo, useState } from "react";
+import organizationData from "../database/organization";
 import OrganizationCard from "../components/OrganizationCard";
+import { Box, Button, CircularProgress, Container, Grid, ListItem, Paper, Skeleton, TextField, Typography } from "@mui/material";
+import { useInfiniteQuery } from "@tanstack/react-query";
+
+const { asyncFetchOrganizations } = organizationData;
 
 export default function IndividualList() {
-
-    const [organizations, setOrganizations] = useState([]);
-    const [query, setQuery] = useState("");
-    const [page, setPage] = useState(1);
-    const [isLoading, setIsLoading] = useState(false);
-
+    const [query, setQuery] = useState(" ");
+    const deferredQuery = useDeferredValue(query, { timeoutMs: 1000 });
     useEffect(() => {
-        if (page === -1) return;
-        setIsLoading(true);
-        fetchOrganizations({ query, page }).then((data) => {
-            setOrganizations(data);
-            setIsLoading(false);
-        });
-    }, [query, page]);
+        setQuery("");
+    }, []);
 
-    return (
-        <>
-            <h1>Organizations</h1>
-            <input value={query} onChange={(e) => setQuery(e.target.value)} />
-            {isLoading && <h2> Loading...</h2>}
-            <ul>
-                {organizations.map((organization) => (
-                    <li key={organization.legal_name}>
-                        <OrganizationCard organization={organization} isLoading={isLoading} />
-                    </li>
+    const { data, fetchNextPage, hasNextPage, isFetching, isFetchingNextPage, isLoading, isError, error } = useInfiniteQuery({
+        queryKey: ["organizations", deferredQuery],
+        queryFn: ({ pageParam }) => asyncFetchOrganizations({ query: deferredQuery, page: pageParam + 1 || 1 }),
+        getNextPageParam: (lastPage, pages) => {
+            if (lastPage.length < 10) {
+                return null;
+            }
+            return pages.length;
+        }
+    });
+
+    const organizations = useMemo(() => {
+        if (data) {
+            return data.pages.flatMap((page) => page);
+        }
+        return [];
+    }, [data]);
+
+    return (<Container maxWidth="lg" sx={{ display: "flex", flexDirection: "column", alignItems: "center", gap: "2rem" }}>
+        <Typography variant="h1">Organizations</Typography>
+        <Box border={1} p={2} borderRadius={2} sx={{ display: "flex", flexDirection: "row", alignItems: "center", gap: "1rem" }}>
+            <TextField
+                label="Search"
+                variant="outlined"
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
+                focused
+            />
+            {isFetching && <CircularProgress color="secondary" />}
+        </Box>
+        <Paper elevation={3} style={{ padding: '1rem', minWidth: "80vw" }}>
+            {isLoading && !data && <Skeleton variant="rectangular" height={600} />}
+            {isError && (
+                <Typography variant="h2" color="error">
+                    Error: {error.message}
+                </Typography>
+            )}
+            <Grid container spacing={1}>
+                {organizations.map((id) => (
+                    <Grid item key={id} xs={12} sm={6}>
+                        <ListItem>
+                            <OrganizationCard id={id} isLoadingData={isLoading} />
+                        </ListItem>
+                    </Grid>
                 ))}
-            </ul>
-            <button onClick={() => setPage((page) => (page + 1))} disabled={page === -1} > Next Page </button>
-            <button onClick={() => setPage((page) => (page - 1))} disabled={page === 1} > Previous Page </button>
-        </>);
+            </Grid>
+            <Button
+                onClick={() => fetchNextPage()}
+                disabled={!hasNextPage || isFetchingNextPage}
+                variant="contained"
+                color="primary"
+                fullWidth={true}
+                sx={{ marginTop: "1rem" }}
+                onMouseOver={() => {
+                    if (!hasNextPage || isFetchingNextPage)
+                        return;
+                    fetchNextPage();
+                }}
+            >
+                {isFetchingNextPage ?
+                    <Typography variant="h5" color="info.main">
+                        Loading more...
+                    </Typography>
+                    : hasNextPage ? <Typography variant="h5">Load More</Typography>
+                        : <Typography variant="h5">Nothing more to load</Typography>
+                }
+            </Button>
+        </Paper>
+    </Container>
+    );
 }
