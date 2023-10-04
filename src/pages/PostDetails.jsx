65cd6eef9d4551dad875b9f019ee183c04dbd691
VishalDhoriya <202101446@daiict.ac.in>
2023-10-04 10:59:56 +0530

    add mui-components and styling to post-details page

--- src/pages/PostDetails.jsx
+++ src/pages/PostDetails.jsx
@@ -2,22 +2,23 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import PostsData from '../database/post';
 import {
-    Avatar,
+  Avatar,
   Box,
-  Button,
   CircularProgress,
   Container,
   Paper,
   Typography,
+  Divider,
 } from '@mui/material';
 
+
 function PostDetail() {
-  const { postId } = useParams(); 
+  const { postId } = useParams();
   const [Post, setPost] = useState(null);
   const [error, setError] = useState(null);
+   
 
   useEffect(() => {
-   
     PostsData.fetchPost(postId)
       .then((postsData) => {
         setPost(postsData);
@@ -50,35 +51,47 @@ function PostDetail() {
   }
 
   return (
-    <Container maxWidth="xl">
-      <Paper elevation={3} style={{ padding: '2rem' }}>
-        <Box display="flex" alignItems="center">
-          <Avatar src={`${Post.post_logo}`} sx={{ width: 70, height: 70 }} variant="rounded" />
-          <Box marginLeft="1rem">
-            <Typography variant="h6">
-              {Post.post_first_name} {Post.post_last_name}
-            </Typography>
-            <Typography variant="subtitle1">
-              {`${Post.posted_on.toLocaleDateString()}` + " " + `${Post.post_hour}` + ":" + `${Post.post_min}` + " " + `${Post.post_ap}`}
-            </Typography>
+    <Container maxWidth="md">
+      <Box
+        sx={{
+          backgroundColor: '#f5f5f5',
+          padding: '1rem', 
+        }}
+      >
+        <Paper elevation={3} style={{
+          padding: '2rem',
+          backgroundColor: '#f5f5f5', 
+          backgroundImage: 'radial-gradient(2px 2px at 50px 200px, #eee, rgba(0, 0, 0, 0)), radial-gradient(2px 2px at 40px 70px, #fff, rgba(0, 0, 0, 0)), radial-gradient(3px 4px at 120px 40px, #ddd, rgba(0, 0, 0, 0))', // Background image
+          backgroundRepeat: 'repeat, repeat, repeat',
+          backgroundSize: '200px 200px, 200px 200px, 200px 200px',
+          height: '100%', 
+        }}>
+          <Box display="flex" alignItems="center">
+            <Avatar src={Post.post_logo} sx={{ width: 70, height: 70 }} variant="rounded" />
+            <Box marginLeft="1rem">
+              <Typography variant="h6">
+                {Post.post_first_name} {Post.post_last_name}
+              </Typography>
+              <Typography variant="subtitle1">
+                {`${Post.posted_on.toLocaleDateString()} ${Post.post_hour}:${Post.post_min} ${Post.post_ap}`}
+              </Typography>
+            </Box>
           </Box>
-        </Box>
-        <Typography variant="h6">
+          <Divider sx={{ margin: '1rem 0' }} />
+          <Typography variant="h6">{Post.post_long_description}</Typography>
           <img
-            src={`${Post.post_img}`}
-            alt="Fake Image"
+            src={Post.post_img}
+            alt="Post Image"
             style={{
-                padding: '2rem',
-              display: 'block', // Ensures block-level rendering
-              margin: '0 auto', // Centers the image horizontally
-              maxWidth: '100%', // Ensures the image doesn't exceed its container's width
-              textAlign: 'center', // Centers the image vertically
-            }} />
-        </Typography>
-        <Typography variant="h6">
-          {Post.post_long_description}
-        </Typography>
-      </Paper>
+              display: 'block',
+              margin: '1rem auto',
+              maxWidth: '100%',
+              borderRadius: '8px',
+            }}
+          />
+         
+        </Paper>
+      </Box>
     </Container>
   );
 }
