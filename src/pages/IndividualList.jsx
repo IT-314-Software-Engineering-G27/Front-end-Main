7c7692161119081662bfe96b19373934fc6fbee2
VishalDhoriya <202101446@daiict.ac.in>
2023-10-04 10:23:08 +0530

    add new search bar

--- src/pages/IndividualList.jsx
+++ src/pages/IndividualList.jsx
@@ -4,6 +4,7 @@ import IndividualData from "../database/individual";
 import { useDeferredValue } from "react";
 import { useInfiniteQuery } from "@tanstack/react-query";
 import { Box, Button, CircularProgress, Container, Grid, ListItem, Paper, Skeleton, TextField, Typography } from "@mui/material";
+import ClearIcon from "@mui/icons-material/Clear";
 
 const { asyncFetchIndividuals } = IndividualData;
 
@@ -14,6 +15,10 @@ export default function IndividualList() {
         setQuery("");
     }, []);
 
+    const handleClear = () => {
+        setQuery("");
+      };
+      
     const { data, fetchNextPage, hasNextPage, isFetching, isFetchingNextPage, isLoading, isError, error } = useInfiniteQuery({
         queryKey: ["organizations", deferredQuery],
         queryFn: ({ pageParam }) => asyncFetchIndividuals({ query: deferredQuery, page: pageParam + 1 || 1 }),
@@ -32,57 +37,135 @@ export default function IndividualList() {
         return [];
     }, [data]);
 
-    return (
-        <Container maxWidth="lg" sx={{ display: "flex", flexDirection: "column", alignItems: "center", gap: "2rem" }}>
+    return (  <Container
+        maxWidth="lg"
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          gap: "2rem",
+        }}
+      >
         <Typography variant="h1">Individuals</Typography>
-        <Box border={1} p={2} borderRadius={2} sx={{ display: "flex", flexDirection: "row", alignItems: "center", gap: "1rem" }}>
+        <Box
+          p={2}
+          borderRadius={2}
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            gap: "1rem",
+            background: "#f0f4f8",
+            width: "80rem",
+          }}
+        >
+          <Box
+            p={2}
+            borderRadius={2}
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+              gap: "1rem",
+              background: "#fff",
+              padding: "0.5rem",
+              width: "100%", 
+            }}
+          >
             <TextField
-                label="Search"
-                variant="outlined"
-                value={query}
-                onChange={(e) => setQuery(e.target.value)}
-                focused
+              placeholder="Search here..."
+              variant="outlined"
+              value={query}
+              onChange={(e) => setQuery(e.target.value)}
+              focused
+              fullWidth
+              InputProps={{
+                endAdornment: query && (
+                  <ClearIcon
+                    onClick={handleClear}
+                    style={{ cursor: "pointer" }}
+                  />
+                ),
+                sx: {
+                  "& .MuiOutlinedInput-notchedOutline": {
+                    border: "none",
+                  },
+                  "& .MuiInputBase-input": {
+                    background: "white",
+                  },
+                },
+              }}
             />
-            {isFetching && <CircularProgress color="secondary" />}
-        </Box>
-        <Paper elevation={3} style={{ padding: '1rem', minWidth: "80vw" }}>
-            {isLoading && !data && <Skeleton variant="rectangular" height={600} />}
-            {isError && (
-                <Typography variant="h2" color="error">
-                    Error: {error.message}
-                </Typography>
+            {isFetching && (
+              <CircularProgress
+                color="secondary"
+                sx={{ marginLeft: "auto" }} 
+              />
             )}
-            <Grid container spacing={1}>
-                {individuals.map((id) => (
-                    <Grid item key={id} xs={12} sm={6}>
-                        <ListItem>
-                            <IndividualCard id={id} isLoadingData={isLoading} />
-                        </ListItem>
-                    </Grid>
-                ))}
-            </Grid>
-            <Button
-                onClick={() => fetchNextPage()}
-                disabled={!hasNextPage || isFetchingNextPage}
-                variant="contained"
-                color="primary"
-                fullWidth={true}
-                sx={{ marginTop: "1rem" }}
-                onMouseOver={() => {
-                    if (!hasNextPage || isFetchingNextPage)
-                        return;
-                    fetchNextPage();
-                }}
-            >
-                {isFetchingNextPage ?
-                    <Typography variant="h5" color="info.main">
-                        Loading more...
-                    </Typography>
-                    : hasNextPage ? <Typography variant="h5">Load More</Typography>
-                        : <Typography variant="h5">Nothing more to load</Typography>
-                }
-            </Button>
+          </Box>
+        </Box>
+        <Paper
+          elevation={3}
+          style={{
+            padding: "2rem",
+            background: "rgba(255, 255, 255, 0.9)",
+            borderRadius: "8px",
+            boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)",
+          }}
+        >
+          {isLoading && !data && <Skeleton variant="rectangular" height={600} />}
+          {isError && (
+            <Typography variant="h2" color="error">
+              Error: {error.message}
+            </Typography>
+          )}
+          <Grid container spacing={3}>
+            {individuals.map((id) => (
+              <Grid item key={id} xs={12} sm={6} md={4}>
+                <IndividualCard id={id} isLoadingData={isLoading} />
+              </Grid>
+            ))}
+          </Grid>
+         
+  
+  
+          <Button
+    onClick={() => fetchNextPage()}
+    disabled={!hasNextPage || isFetchingNextPage}
+    variant="contained"
+    color="primary"
+    fullWidth={true}
+    sx={{
+      marginTop: "1rem",
+      background: "#007bff",
+      color: "#fff",
+      padding: "5px 15px", 
+      border: "none",
+      borderRadius: "5px",
+      cursor: "pointer",
+      transition: "background-color 0.3s ease",
+      "&:hover": {
+        backgroundColor: "#0056b3",
+      },
+      display: "flex", 
+      justifyContent: "center", 
+    }}
+  >
+    {isFetchingNextPage ? (
+      <Typography variant="h5" color="info.main">
+        Loading more...
+      </Typography>
+    ) : hasNextPage ? (
+      <Typography variant="h6">Load More</Typography> 
+    ) : (
+      <Typography variant="h6">Nothing more to load</Typography> 
+    )}
+  </Button>
+  
+  
+  
         </Paper>
-    </Container>
+      </Container>
     );
-}
+  }
+  
