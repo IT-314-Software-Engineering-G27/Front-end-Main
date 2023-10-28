481448210a1339f00ef69d54dbe2c5686f99d9fc
MeeraPanchal74 <meerapanchal1494@gmail.com>
2023-10-28 13:08:32 +0530

    comapny  job Registration page added

--- src/pages/CompanyJobRegistration.jsx
+++ src/pages/CompanyJobRegistration.jsx
@@ -1,37 +1,12 @@
 import { Paper,TextField,Grid ,Button} from "@mui/material";
 import {useRef , useState} from "react";
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import InputAdornment from '@mui/material/InputAdornment';
+import FormControl from '@mui/material/FormControl';
 
 export default function CompanyJobRegistration(){
-   const inputRef1= useRef(null);
-   const inputRef2= useRef(null);
-   const inputRef3= useRef(null);
-   const [image1,setImage1] = useState("");
-   const [image2,setImage2] = useState("");
-   const [image3,setImage3] = useState("");
-
-   const handleImageClick1= () =>{
-       inputRef1.current.click();
-   }
-   const handleImageClick2 = () =>{
-       inputRef2.current.click();
-   }
-   const handleImageClick3 = () =>{
-       inputRef3.current.click();
-   }
-
-   const handleImageChange1= (event) =>{
-       const file1= event.target.files[0];
-       setImage1(file1);
-       }
-
-   const handleImageChange2= (event) =>{
-       const file2= event.target.files[0];
-       setImage2(file2);
-       }
-   const handleImageChange3= (event) =>{
-       const file3= event.target.files[0];
-       setImage3(file3);
-        }
+   
 
    return(
        <div style={{display:"flex",
@@ -39,7 +14,7 @@ export default function CompanyJobRegistration(){
            justifyContent:"center",
            alignItems:"center"
        }}>
-       <h1>Event Registration</h1>
+       <h1>Company's Job Registration</h1>
        <Paper elevation={10} 
        style ={{
            margin: "30px",
@@ -53,70 +28,39 @@ export default function CompanyJobRegistration(){
            padding:"30px",
            gap:"15px"
        }}>
-               
+                <TextField
+               required
+               id="outlined-required"
+               label="Comapany's name"
+               defaultValue="Enter the name of your Company"
+               style={{margin: "5px", width:"75%"}}
+               />
                <TextField
                required
                id="outlined-required"
-               label="Startup Idea"
-               defaultValue="Type the Idea here.."
+               label="Job Profile"
+               defaultValue="Enter the Job Profile"
                style={{margin: "5px", width:"75%"}}
                />
+               
            <TextField
                id="outlined-multiline-static"
-               label="Idea Description"
+               label="Job Description"
                multiline
                rows={6}
                defaultValue="Write the description here.."
                style={{margin: "5px", width:"75%"}}
            />
-
-           <h4>Click on the image to add a new image</h4>
-           <div style={{width:"75%",
-               display:"flex",
-               justifyContent:"center"
-           }}>
+            <FormControl fullWidth sx={{ m: 1 }}  style={{width:"75%"}}>
+          <InputLabel htmlFor="outlined-adornment-amount" >Salary</InputLabel>
+          <OutlinedInput
+            id="outlined-adornment-amount"
+            startAdornment={<InputAdornment position="start">$</InputAdornment>}
+            label="Amount"
            
-           <Grid container spacing={11}  style={{
-               margin: "5px", 
-               width:"100%" , 
-               display:"flex",
-               alignItems:"center",
-              
-               }}>
-
-               <Grid xs={4} style={{
-               display:"flex",
-               justifyContent:"center",
-               //  margin:"30px"
-               }}
-               onClick ={handleImageClick1}
-               >
-                {/* <img src="https://cdn2.iconfinder.com/data/icons/design-development-7/512/022-add_image-512.png" style={{width:"100%"}}/> */}
-                {image1 ?(<img src={URL.createObjectURL(image1)} style={{height:"250px" , width:"250px"}}/>):(<img src="https://cdn2.iconfinder.com/data/icons/design-development-7/512/022-add_image-512.png" style={{width:"100%"}}/>)}
-                <input type="file" ref={inputRef1} onChange={handleImageChange1} style={{display:"none"}}></input>
-               </Grid>
-
-               <Grid xs={4} style={{
-               display:"flex",
-               justifyContent:"center"}}
-               onClick ={handleImageClick2}
-               >
-                   {/* <img src="https://cdn2.iconfinder.com/data/icons/design-development-7/512/022-add_image-512.png" style={{width:"100%"}}></img> */}
-                   {image2 ?(<img src={URL.createObjectURL(image2)} style={{height:"250px" , width:"250px"}}/>):(<img src="https://cdn2.iconfinder.com/data/icons/design-development-7/512/022-add_image-512.png" style={{width:"100%"}}/>)}
-                   <input type="file" ref={inputRef2} onChange={handleImageChange2} style={{display:"none"}}></input>
-               </Grid>
-
-               <Grid xs={4} style={{
-               display:"flex",
-               justifyContent:"center"}}
-               onClick ={handleImageClick3}
-               >
-                   {/* <img src="https://cdn2.iconfinder.com/data/icons/design-development-7/512/022-add_image-512.png" style={{width:"100%"}}></img> */}
-                   {image3 ?(<img src={URL.createObjectURL(image3)} style={{height:"250px" , width:"250px"}}/>):(<img src="https://cdn2.iconfinder.com/data/icons/design-development-7/512/022-add_image-512.png" style={{width:"100%"}}/>)}
-                   <input type="file" ref={inputRef3} onChange={handleImageChange3} style={{display:"none"}}></input>
-               </Grid>
-           </Grid>
-           </div>
+          />
+        </FormControl>
+          
            <Button variant="contained" style={{width:" 200px",border:"solid white 1px", borderRadius:"5px"}}>Submit</Button>
        </Paper>
        </div>
