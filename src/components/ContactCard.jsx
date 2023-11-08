fc2006a74d035e3588d653f4991891b4ce809005
sohamviradiya <sohamviradiya2003@gmail.com>
2023-11-08 18:41:48 +0530

    change date type

--- src/components/ContactCard.jsx
+++ src/components/ContactCard.jsx
@@ -14,22 +14,22 @@ export default function ContactCard({ id, token }) {
     return (
         <>
             <Box sx={{ display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "space-between", width: "100%", borderRadius: "5px", boxShadow: " 5px 5px rgba(0, 0, 0, 0.15)", border: "1px solid black", padding: "0.8rem" }}>
-                {!contact ? 
+                {!contact ?
                     <>
-                        <Skeleton variant="circular" width={40} height={40} sx={{marginRight: "20px"}} />
+                        <Skeleton variant="circular" width={40} height={40} sx={{ marginRight: "20px" }} />
                         <Skeleton variant="rectangular" width={200} height={40} />
-                </>
-                 : <>
-                    <Box sx={{ display: "flex", flexDirection: "row", alignItems: "center" }}>
-                        <Avatar sx={{ marginRight: "0.5rem" }} src={contact.recipient.profile_image} />
-                    </Box>
-                    <Box sx={{ display: "flex", flexDirection: "column", width: "100%" }} component={Link} to={`/contacts/${contact._id}`}>
-                        <Typography variant="body1">{contact.recipient.username}</Typography>
-                        <Typography variant="body2" sx={{ display: "flex", alignItems: "center" }}>
-                            Last seen: {contact?.last_seen?.toLocaleString() || "Never"}
-                        </Typography>
-                    </Box>
-                </>}
+                    </>
+                    : <>
+                        <Box sx={{ display: "flex", flexDirection: "row", alignItems: "center" }}>
+                            <Avatar sx={{ marginRight: "0.5rem" }} src={contact.recipient.profile_image} />
+                        </Box>
+                        <Box sx={{ display: "flex", flexDirection: "column", width: "100%" }} component={Link} to={`/contacts/${contact._id}`}>
+                            <Typography variant="body1">{contact.recipient.username}</Typography>
+                            <Typography variant="body2" sx={{ display: "flex", alignItems: "center" }}>
+                                Last seen: {new Date(contact?.last_seen).toLocaleString()  || "Never"}
+                            </Typography>
+                        </Box>
+                    </>}
             </Box>
 
         </>
