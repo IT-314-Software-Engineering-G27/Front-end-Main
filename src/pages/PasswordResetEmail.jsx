35e69c4d198b133815c46d8de295c5f0de661dab
sohamviradiya <sohamviradiya2003@gmail.com>
2023-11-08 20:50:35 +0530

    link password reset pages

--- src/pages/PasswordResetEmail.jsx
+++ src/pages/PasswordResetEmail.jsx
@@ -1,26 +1,11 @@
 import React, { useState } from 'react';
 import { Button, TextField, Container, Typography, Grid } from '@mui/material';
-import { useNavigate } from 'react-router-dom';
 
 export default function PasswordResetEmail() {
   const [email, setEmail] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [success, setSuccess] = useState(false);
   const [emailError, setEmailError] = useState('');
-  const navigate = useNavigate();
-
-  const handleStep1Submit = () => {
-    if (!validateEmail(email)) {
-      setEmailError('Invalid email address');
-      return;
-    }
-    else {
-      setEmailError('');
-      navigate('PasswordResetOTP');
-    }
-  };
-  const validateEmail = (email) => {
-    const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-    return emailPattern.test(email);
-  };
 
   return (
     <Container maxWidth="md">
@@ -36,6 +21,7 @@ export default function PasswordResetEmail() {
               label="Email"
               variant="outlined"
               fullWidth
+              focused
               value={email}
               onChange={(e) => {
                 setEmail(e.target.value);
@@ -48,15 +34,53 @@ export default function PasswordResetEmail() {
             <Button
               variant="contained"
               color="primary"
-              onClick={handleStep1Submit}
+              onClick={() => {
+                if (!validateEmail(email)) {
+                  setEmailError('Invalid email address');
+                  return;
+                }
+                else {
+                  setEmailError('');
+                  setLoading(true);
+                  submitPasswordResetRequest(email).then(({ error }) => {
+                    if (error) setEmailError(error);
+                    setSuccess(true);
+                    setLoading(false);
+                  }).catch((err) => {
+                    console.log(err);
+                    setLoading(false);
+                  });
+                }
+              }}
               fullWidth
               sx={{ marginTop: 2 }}
+              disabled={loading || success}
             >
-              SUBMIT
+              {loading ? 'Loading...' : (success ? 'Check your mail' : 'Send Email')}
             </Button>
           </form>
         </Grid>
       </Grid>
     </Container>
   )
+}
+
+function validateEmail(email) {
+  const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+  return emailPattern.test(email);
+};
+
+async function submitPasswordResetRequest(email) {
+  const response = await fetch('http://localhost:5000/reset', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ email })
+  });
+  const data = await response.json();
+  if (!response.ok) {
+    return { error: data.message };
+  }
+  return data;
 }
--- src/pages/PasswordResetOTP.jsx
+++ src/pages/PasswordResetOTP.jsx
@@ -1,20 +1,13 @@
 import React, { useState } from 'react';
 import { Button, TextField, Container, Typography, Grid } from '@mui/material';
-
+import { useNavigate, useParams } from 'react-router-dom';
 export default function PasswordResetOTP() {
+  const { resetId } = useParams();
+  const navigate = useNavigate();
   const [otp, setOtp] = useState('');
   const [newPassword, setNewPassword] = useState('');
-  const [otpError, setOtpError] = useState('');
-  const handleStep2Submit = () => {
-    if (!validateOTP(otp)) {
-      setOtpError('Invalid OTP');
-      return;
-    }
-    else { setOtpError(''); }
-  };
-  const validateOTP = (otp) => {
-    return otp === '123456';
-  };
+  const [error, setError] = useState('');
+  const [loading, setLoading] = useState(false);
 
   return (
     <Container maxWidth="md">
@@ -33,11 +26,11 @@ export default function PasswordResetOTP() {
               value={otp}
               onChange={(e) => {
                 setOtp(e.target.value);
-                setOtpError('');
+                setError('');
               }}
               sx={{ marginTop: 2 }}
+              focused
             />
-            {otpError && <Typography color="error">{otpError}</Typography>}
             <TextField
               label="New Password"
               type="password"
@@ -46,19 +39,55 @@ export default function PasswordResetOTP() {
               value={newPassword}
               onChange={(e) => setNewPassword(e.target.value)}
               sx={{ marginTop: 1 }}
+              focused
             />
+            {error && <Typography color="error">{error}</Typography>}
             <Button
               variant="contained"
               color="primary"
-              onClick={handleStep2Submit}
+              onClick={() => {
+                if (!validatePassword(newPassword)) {
+                  setError('Password must contain at least 8 characters, one uppercase, one lowercase, one number and one special character');
+                  return;
+                };
+                setLoading(true);
+                submitNewPassword({ resetId, otp, password: newPassword }).then(({ error }) => {
+                  if (error) setError(error);
+                  else navigate('/login');
+                  setLoading(false);
+                }).catch((err) => {
+                  console.log(err);
+                  setLoading(false);
+                });
+              }}
               fullWidth
               sx={{ marginTop: 2 }}
             >
-              Reset Password
+              {loading ? 'Loading...' : 'Submit'}
             </Button>
           </form>
         </Grid>
       </Grid>
     </Container>
   )
+}
+
+function validatePassword(password) {
+  const passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+  return passwordPattern.test(password);
+};
+
+async function submitNewPassword({ resetId, otp, password }) {
+  const response = await fetch(`http://localhost:5000/reset/${resetId}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ otp, password })
+  });
+  const data = await response.json();
+  if (!response.ok) {
+    return { error: data.message };
+  }
+  return data;
 }
