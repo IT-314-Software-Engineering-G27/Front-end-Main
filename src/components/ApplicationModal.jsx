152e0211caef7a76687da5cafc7c30e66aa762fb
sohamviradiya <sohamviradiya2003@gmail.com>
2023-11-10 18:00:47 +0530

    fix formatting in ApplicationModal component

--- src/components/ApplicationModal.jsx
+++ src/components/ApplicationModal.jsx
@@ -3,33 +3,40 @@ import { Button, FormControl, FormGroup, Modal, TextField } from "@mui/material"
 import { useAuth } from "../contexts/session";
 import { Link } from "react-router-dom";
 import { API_URL } from "../config";
-const buttonStyle = { width: '80%',margin: 'auto', alignItems: 'center', transition: 'background-color 0.3s, transform 0.3s', boxShadow: " 5px 5px rgba(163, 23, 205, 0.1)", '&:hover': { backgroundColor: '#1976D2', transform: 'scale(1.05)', },};
+const buttonStyle = { width: '80%', margin: 'auto', alignItems: 'center', transition: 'background-color 0.3s, transform 0.3s', boxShadow: " 5px 5px rgba(163, 23, 205, 0.1)", '&:hover': { backgroundColor: '#1976D2', transform: 'scale(1.05)', }, };
 
 function ApplicationButton({ id }) {
     const [open, setOpen] = useState(false);
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
     const [status, setStatus] = useState("none");
+    const [individual, setIndividual] = useState(null);
     const [jobApplication, setJobApplication] = useState(null);
     const auth = useAuth();
     useEffect(() => {
-        fetchApplicationStatus({ id, token: auth.session.token }).then((jobApplication) => {
-            setStatus(jobApplication.status);
-            setJobApplication(jobApplication);
-        });
-    }, [auth.session.token, id]);
+        if (auth?.session?.user?.individual && auth?.session?.token) {
+            fetchApplicationStatus({ id, token: auth.session.token }).then((jobApplication) => {
+                setStatus(jobApplication.status);
+                setJobApplication(jobApplication);
+            });
+            fetchProfile({ id: auth.session.user.individual }).then((individual) => {
+                setIndividual(individual);
+            });
+        }
+    }, [auth, id]);
 
     if (status === "none")
-        return <Button variant="contained"  sx={{ ...buttonStyle,  }}> Loading...  </Button>;
+        return <Button variant="contained" sx={{ ...buttonStyle, }}> Loading...  </Button>;
 
     if (status === "not-logged-in")
-        return <Button variant="contained"  sx={{ ...buttonStyle,  }} LinkComponent={Link} to="/login"> Login to apply </Button>;
+        return <Button variant="contained" sx={{ ...buttonStyle, }} LinkComponent={Link} to="/login"> Login to apply </Button>;
 
 
     if (status === "pending")
         return (<>
-            <ApplicationModal open={open} handleClose={handleClose} id={id} application={jobApplication} edit />
-            <Button variant="contained" sx={{ ...buttonStyle, }} onClick={handleOpen}> Edit Application </Button></>);
+            <ApplicationModal open={open} handleClose={handleClose} id={id} application={jobApplication} edit individual={individual} />
+            <Button variant="contained" sx={{ ...buttonStyle, }} onClick={handleOpen} > Edit Application </Button>
+        </>);
 
     if (status === "accepted")
         return <Button variant="contained" sx={{ ...buttonStyle, }}> Accepted </Button>;
@@ -38,12 +45,12 @@ function ApplicationButton({ id }) {
         return <Button variant="contained" sx={{ ...buttonStyle }}> Rejected </Button>;
 
     return <>
-        <Button variant="contained" sx={{ ...buttonStyle,  }} onClick={handleOpen}> Apply Now  </Button>
-        <ApplicationModal open={open} handleClose={handleClose} id={id} />
+        <Button variant="contained" sx={{ ...buttonStyle, }} onClick={handleOpen}> Apply Now  </Button>
+        <ApplicationModal open={open} handleClose={handleClose} id={id} individual={individual} />
     </>;
 }
 
-function ApplicationModal({ open, handleClose, id, application, edit }) {
+function ApplicationModal({ open, handleClose, id, application, edit, individual }) {
     const auth = useAuth();
     const [content, setContent] = useState(application?.cover_letter || "");
     return <Modal
@@ -62,22 +69,34 @@ function ApplicationModal({ open, handleClose, id, application, edit }) {
         }}
     >
         <FormControl sx={{ width: "100%", backgroundColor: 'white', padding: "20px" }}>
-            <TextField multiline label="cover letter" rows={12} variant="outlined" sx={{ width: "100%", mb: 2 }} focused onChange={(e) => setContent(e.target.value)} value={content} placeholder="write about your skills, qualifications and achivements"/>
+            <TextField multiline label="profile" rows={8} variant="outlined" sx={{ width: "100%", mb: 2 }} focused disabled value={createProfileFromData({ individual })} />
+            <TextField multiline label="cover letter" rows={12} variant="outlined" sx={{ width: "100%", mb: 2 }} focused onChange={(e) => setContent(e.target.value)} value={content} placeholder="Why should we hire you?" />
             <FormGroup sx={{ display: "flex", justifyContent: "flex-end", flexDirection: "row", gap: "10px" }}>
                 <Button variant="contained" sx={{ ...buttonStyle }} onClick={() => {
                     if (edit)
-                        editApplication({ id: application._id, token: auth.session.token, cover_letter: content }).then(() => {
+                        editApplication({ id: application._id, token: auth.session.token, cover_letter: content + createProfileFromData({ individual }) }).then(() => {
                             window.location.reload();
                         });
                     else
-                        submitApplication({ id, token: auth.session.token, cover_letter: content }).then(() => {
+                        submitApplication({ id, token: auth.session.token, cover_letter: content + createProfileFromData({ individual }) }).then(() => {
                             window.location.reload();
                         })
                 }}> Submit </Button>
-                <Button variant="contained" sx={{ ...buttonStyle,}} onClick={handleClose}> Close </Button>
+                <Button variant="contained" sx={{ ...buttonStyle, }} onClick={handleClose}> Close </Button>
             </FormGroup>
         </FormControl>
     </Modal>;
+}
+
+async function fetchProfile({ id }) {
+    const response = await fetch(`${API_URL}/individuals/${id}`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+        }
+    });
+    const data = await response.json();
+    return data.payload.individual;
 }
 
 async function fetchApplicationStatus({ id, token }) {
@@ -122,6 +141,16 @@ async function editApplication({ id, token, cover_letter }) {
     });
     const data = await response.json();
     return data.payload.jobApplication;
+}
+
+function createProfileFromData({ individual }) {
+    if (!individual) return "";
+    return `
+        name: ${individual.first_name} ${individual.last_name} \n
+        skills: ${individual.skills.join(", ")} \n
+        degree: ${individual.degree} \n
+        college: ${individual.college} \n
+    `
 }
 
 export default ApplicationButton;
