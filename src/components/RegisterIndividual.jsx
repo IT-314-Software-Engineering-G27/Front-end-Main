4b79a4871abef107729a46483e6563ffced8bd8f
avneets2003 <avneets2003@gmail.com>
2023-11-21 03:03:08 +0530

    Added conditions in form

--- src/components/RegisterIndividual.jsx
+++ src/components/RegisterIndividual.jsx
@@ -6,50 +6,101 @@ import {
   Typography,
   TextField,
   Button,
-  Radio,
-  RadioGroup,
-  FormControl,
-  FormControlLabel,
-  FormLabel,
-  Checkbox,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  DialogActions,
 } from '@mui/material';
 import AddCircleOutlineOutlinedIcon from '@mui/icons-material/AddCircleOutlineOutlined';
 
 const RegisterIndividual = () => {
   const [page, setPage] = useState(1);
-
-  const paperStyle = {
-    padding: 20,
-    height: '93.2vh',
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    overflow: 'auto',
-  };
-
-  const headerStyle = { margin: 0 };
-  const avatarStyle = { backgroundColor: '#DED7FD' };
-  const marginTop = { marginTop: 10 };
+  const [errors, setErrors] = useState({});
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    mobileNumber: '',
+    username: '',
+    password: '',
+    college: '',
+    country: '',
+    age: '',
+    highestQualification: '',
+    skills: '',
+  });
+  const [openDialog, setOpenDialog] = useState(false);
+  const [errorDialogText, setErrorDialogText] = useState('');
 
   const textFieldStyle = { marginBottom: 10, width: '100%' };
 
-  const handleNextPage = () => {
-    setPage(page + 1);
+  const handleChange = (event) => {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleDialogClose = () => {
+    setOpenDialog(false);
+    setErrorDialogText('');
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    // Handle form submission logic here
+
+    const emailRegex = /^\S+@\S+\.\S{2,}$/;
+    const phoneRegex = /^\+?\d{1,3}[-.\s]?\d{1,15}$/;
+    const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d@$!%*?&]{8,15}$/;
+
+    let newErrors = {};
+
+    if (!emailRegex.test(formData.email.trim())) {
+      newErrors.email = 'Invalid email format';
+    }
+
+    if (!phoneRegex.test(formData.mobileNumber.trim())) {
+      newErrors.mobileNumber = 'Invalid phone number format';
+    }
+
+    if (!passwordRegex.test(formData.password)) {
+      newErrors.password =
+        'Password must be 8-15 characters, with at least one letter and one digit';
+    }
+
+    setErrors(newErrors);
+
+    if (Object.keys(newErrors).length === 0) {
+      setPage(page + 1);
+    } else {
+      const errorMessage = Object.values(newErrors).join('\n');
+      setErrorDialogText(errorMessage);
+      setOpenDialog(true);
+    }
+
+    if (Object.keys(newErrors).length === 0 && page === 2) {
+      window.location.reload();
+    }
   };
 
   return (
     <Grid>
-      <Paper style={paperStyle}>
+      <Paper
+        style={{
+          padding: 20,
+          height: '93.2vh',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          overflow: 'auto',
+        }}
+      >
         <Grid align="center">
-          <Avatar style={avatarStyle}>
+          <Avatar style={{ backgroundColor: '#DED7FD' }}>
             <AddCircleOutlineOutlinedIcon />
           </Avatar>
-          <h2 style={headerStyle}>For Individuals</h2>
+          <h2 style={{ margin: 0 }}>For Individuals</h2>
           <br />
         </Grid>
         <form onSubmit={handleSubmit}>
@@ -59,41 +110,65 @@ const RegisterIndividual = () => {
                 style={textFieldStyle}
                 label="First Name"
                 placeholder="Enter your first name"
+                name="firstName"
+                value={formData.firstName}
+                onChange={handleChange}
               />
               <TextField
                 style={textFieldStyle}
                 label="Last Name"
                 placeholder="Enter your last name"
+                name="lastName"
+                value={formData.lastName}
+                onChange={handleChange}
               />
               <TextField
                 style={textFieldStyle}
                 label="Email"
                 placeholder="Enter your email address"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
               />
               <TextField
                 style={textFieldStyle}
                 label="Mobile Number"
                 placeholder="Enter your mobile number"
+                name="mobileNumber"
+                value={formData.mobileNumber}
+                onChange={handleChange}
               />
               <TextField
                 style={textFieldStyle}
                 label="Username"
                 placeholder="Enter your username"
+                name="username"
+                value={formData.username}
+                onChange={handleChange}
               />
               <TextField
                 style={textFieldStyle}
                 label="Password"
                 placeholder="Enter your password"
+                type="password"
+                name="password"
+                value={formData.password}
+                onChange={handleChange}
               />
               <TextField
                 style={textFieldStyle}
                 label="Confirm Password"
                 placeholder="Confirm your password"
+                type="password"
+                name="confirmPassword"
+                value={formData.confirmPassword}
+                onChange={handleChange}
               />
               <Button
+                type="submit"
                 variant="contained"
                 color="primary"
-                onClick={handleNextPage}
+                onClick={handleSubmit}
               >
                 Next
               </Button>
@@ -102,39 +177,70 @@ const RegisterIndividual = () => {
 
           {page === 2 && (
             <>
-              {/* Additional form fields for the second page */}
               <TextField
                 style={textFieldStyle}
                 label="College"
                 placeholder="Enter your college name"
+                name="college"
+                value={formData.college}
+                onChange={handleChange}
               />
               <TextField
                 style={textFieldStyle}
                 label="Country"
                 placeholder="Enter your country"
+                name="country"
+                value={formData.country}
+                onChange={handleChange}
               />
               <TextField
                 style={textFieldStyle}
                 label="Age"
                 placeholder="Enter your age"
+                name="age"
+                value={formData.age}
+                onChange={handleChange}
               />
               <TextField
                 style={textFieldStyle}
                 label="Highest Qualification"
                 placeholder="Enter your highest qualification"
+                name="highestQualification"
+                value={formData.highestQualification}
+                onChange={handleChange}
               />
               <TextField
                 style={textFieldStyle}
                 label="Skills"
                 placeholder="Mention your skills separated by commas"
+                name="skills"
+                value={formData.skills}
+                onChange={handleChange}
               />
-              <Button type="submit" variant="contained" color="primary">
+              <Button
+                type="submit"
+                variant="contained"
+                color="primary"
+                onClick={handleSubmit}
+              >
                 Submit
               </Button>
             </>
           )}
         </form>
       </Paper>
+
+      <Dialog open={openDialog} onClose={handleDialogClose}>
+        <DialogTitle>Error</DialogTitle>
+        <DialogContent>
+          <DialogContentText>{errorDialogText}</DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleDialogClose} color="primary">
+            OK
+          </Button>
+        </DialogActions>
+      </Dialog>
     </Grid>
   );
 };
--- src/components/RegisterOrganization.jsx
+++ src/components/RegisterOrganization.jsx
@@ -6,50 +6,98 @@ import {
   Typography,
   TextField,
   Button,
-  Radio,
-  RadioGroup,
-  FormControl,
-  FormControlLabel,
-  FormLabel,
-  Checkbox,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  DialogActions,
 } from '@mui/material';
 import AddCircleOutlineOutlinedIcon from '@mui/icons-material/AddCircleOutlineOutlined';
 
 const RegisterOrganization = () => {
   const [page, setPage] = useState(1);
-
-  const paperStyle = {
-    padding: 20,
-    height: '93.2vh',
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    overflow: 'auto',
-  };
-
-  const headerStyle = { margin: 0 };
-  const avatarStyle = { backgroundColor: '#DED7FD' };
-  const marginTop = { marginTop: 10 };
+  const [errors, setErrors] = useState({});
+  const [formData, setFormData] = useState({
+    legalName: '',
+    email: '',
+    phoneNumber: '',
+    password: '',
+    confirmPassword: '',
+    headquartersLocation: '',
+    ceoName: '',
+    ageOfEstablishment: '',
+  });
+  const [openDialog, setOpenDialog] = useState(false);
+  const [errorDialogText, setErrorDialogText] = useState('');
 
   const textFieldStyle = { marginBottom: 10, width: '100%' };
 
-  const handleNextPage = () => {
-    setPage(page + 1);
+  const handleChange = (event) => {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleDialogClose = () => {
+    setOpenDialog(false);
+    setErrorDialogText('');
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    // Handle form submission logic here
+
+    const emailRegex = /^\S+@\S+\.\S{2,}$/;
+    const phoneRegex = /^\+?\d{1,3}[-.\s]?\d{1,15}$/;
+    const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d@$!%*?&]{8,15}$/;
+
+    let newErrors = {};
+
+    if (!emailRegex.test(formData.email.trim())) {
+      newErrors.email = 'Invalid email format';
+    }
+
+    if (!phoneRegex.test(formData.phoneNumber.trim())) {
+      newErrors.phoneNumber = 'Invalid phone number format';
+    }
+
+    if (!passwordRegex.test(formData.password)) {
+      newErrors.password =
+        'Password must be 8-15 characters, with at least one letter and one digit';
+    }
+
+    setErrors(newErrors);
+
+    if (Object.keys(newErrors).length === 0) {
+      setPage(page + 1);
+    } else {
+      const errorMessage = Object.values(newErrors).join('\n');
+      setErrorDialogText(errorMessage);
+      setOpenDialog(true);
+    }
+
+    if (Object.keys(newErrors).length === 0 && page === 2) {
+      window.location.reload();
+    }
   };
 
   return (
     <Grid>
-      <Paper style={paperStyle}>
+      <Paper
+        style={{
+          padding: 20,
+          height: '93.2vh',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          overflow: 'auto',
+        }}
+      >
         <Grid align="center">
-          <Avatar style={avatarStyle}>
+          <Avatar style={{ backgroundColor: '#DED7FD' }}>
             <AddCircleOutlineOutlinedIcon />
           </Avatar>
-          <h2 style={headerStyle}>For Organizations</h2>
+          <h2 style={{ margin: 0 }}>For Organizations</h2>
           <br />
         </Grid>
         <form onSubmit={handleSubmit}>
@@ -58,37 +106,50 @@ const RegisterOrganization = () => {
               <TextField
                 style={textFieldStyle}
                 label="Legal Name"
-                placeholder="Enter your company's name"
+                placeholder="Enter your legal name"
+                name="legalName"
+                value={formData.legalName}
+                onChange={handleChange}
               />
               <TextField
                 style={textFieldStyle}
                 label="Email"
                 placeholder="Enter your email address"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
               />
               <TextField
                 style={textFieldStyle}
                 label="Phone Number"
                 placeholder="Enter your phone number"
-              />
-              <TextField
-                style={textFieldStyle}
-                label="Username"
-                placeholder="Enter your username"
+                name="phoneNumber"
+                value={formData.phoneNumber}
+                onChange={handleChange}
               />
               <TextField
                 style={textFieldStyle}
                 label="Password"
                 placeholder="Enter your password"
+                type="password"
+                name="password"
+                value={formData.password}
+                onChange={handleChange}
               />
               <TextField
                 style={textFieldStyle}
                 label="Confirm Password"
                 placeholder="Confirm your password"
+                type="password"
+                name="confirmPassword"
+                value={formData.confirmPassword}
+                onChange={handleChange}
               />
               <Button
+                type="submit"
                 variant="contained"
                 color="primary"
-                onClick={handleNextPage}
+                onClick={handleSubmit}
               >
                 Next
               </Button>
@@ -97,34 +158,54 @@ const RegisterOrganization = () => {
 
           {page === 2 && (
             <>
-              {/* Additional form fields for the second page */}
               <TextField
                 style={textFieldStyle}
-                label="Headquarter Location"
-                placeholder="Enter your headquarter location"
+                label="Headquarters Location"
+                placeholder="Enter headquarters location"
+                name="headquartersLocation"
+                value={formData.headquartersLocation}
+                onChange={handleChange}
               />
               <TextField
                 style={textFieldStyle}
-                label="Name of CEO"
-                placeholder="Enter the name of your CEO"
+                label="CEO Name"
+                placeholder="Enter CEO name"
+                name="ceoName"
+                value={formData.ceoName}
+                onChange={handleChange}
               />
               <TextField
                 style={textFieldStyle}
-                label="Age"
-                placeholder="Enter your age"
+                label="Age of Establishment"
+                placeholder="Enter age of establishment"
+                name="ageOfEstablishment"
+                value={formData.ageOfEstablishment}
+                onChange={handleChange}
               />
-              <TextField
-                style={textFieldStyle}
-                label="Year of Establishment"
-                placeholder="Enter the year of your establishment"
-              />
-              <Button type="submit" variant="contained" color="primary">
+              <Button
+                type="submit"
+                variant="contained"
+                color="primary"
+                onClick={handleSubmit}
+              >
                 Submit
               </Button>
             </>
           )}
         </form>
       </Paper>
+
+      <Dialog open={openDialog} onClose={handleDialogClose}>
+        <DialogTitle>Error</DialogTitle>
+        <DialogContent>
+          <DialogContentText>{errorDialogText}</DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleDialogClose} color="primary">
+            OK
+          </Button>
+        </DialogActions>
+      </Dialog>
     </Grid>
   );
 };
