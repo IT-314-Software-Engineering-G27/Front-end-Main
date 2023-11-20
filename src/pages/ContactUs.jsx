25c181cb4221029d5d650bc749490e2c6c4f7487
VishalDhoriya <202101446@daiict.ac.in>
2023-11-20 22:57:32 +0530

    Update ContactUs styling

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -12,7 +12,7 @@ const ContactUs = () => {
                   sx={{pt:'32px',mx:{xs:'6px',sm:'24px',md:'32px',lg:'40px'},color:'white', display:'flex',flexDirection:'column'}}
                 >   
                     <Box 
-                     sx={{mb:4,fontSize:{xs:'30px',sm:'60px'},fontWeight:'medium',textAlign:'center'}}
+                     sx={{mb:4,fontSize:{xs:'30px',sm:'60px'},fontWeight:'medium',textAlign:'center',fontFamily:'helvetica', fontWeight:600}}
                     >
                         Contact Us
                     </Box>
@@ -20,7 +20,7 @@ const ContactUs = () => {
 
                     <Box 
                     
-                    sx={{ display: 'flex', flexDirection: { xs:'column',sm: 'column', md: 'row' }, backgroundColor: 'rgba(0, 0, 0, 0.5)', backdropFilter: 'blur(8px)', borderRadius: '20px', padding: 6, marginBottom: 20, fontSize: { xs: 'sm', md: 'base' }, }} >
+                    sx={{ display: 'flex', flexDirection: { xs:'column',sm: 'column', md: 'row' }, backgroundColor: 'rgba(0, 0, 0, 0.2)', backdropFilter: 'blur(8px)', borderRadius: '20px', padding: 6, marginBottom: 20, fontSize: { xs: 'sm', md: 'base' }, }} >
                         
                             <Box sx={{width:{xs:'100%',sm:'100%',md:'40%'},pt:{md:20},pl:{sm:1,md:4,xl:6},pr:{md:10}, justifyItems:'center'}}>
                            
@@ -55,7 +55,7 @@ const ContactUs = () => {
      <iframe width='100%' height='270px' title="DA-IICT Location" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14669.989595290104!2d72.6289155!3d23.188537!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395c2a3c9618d2c5%3A0xc54de484f986b1fa!2sDA-IICT!5e0!3m2!1sen!2sin!4v1678437146026!5m2!1sen!2sin" allowfullscreen="" loading="lazy" ></iframe>
                           
     </Box>
-     <Box sx={{ fontSize: { xs: '16px', md: 'sm' }, marginTop: 3,marginBottom: 3,}}>
+     <Box sx={{color:'black', fontSize: { xs: '20px', md: 'sm' }, marginTop: 3,marginBottom: 3,fontWeight:'600'}}>
             Dhirubhai Ambani Institute Of Information And Communication Technology<br/>
             Near Indroda Circle<br/>
             Gandhinagar - 382007<br/>
@@ -63,10 +63,10 @@ const ContactUs = () => {
      </Box>
      <Divider sx={{ mx: 'auto', borderColor: 'white', my: 1 }} />
                               
-     <Box sx={{textAlign:'center'}}>
-         <Link href="some_email.com" underline='hover' sx={{mb:1/2, color:'white', justifyContent:'center',}}> our_sites_address.com</Link>
+     <Box sx={{textAlign:'center', fontWeight:600,fontSize:'20px'}}>
+         <Link href="https://startapp-for-startups-g27.vercel.app/home" underline='hover' sx={{mb:1/2, color:'white', justifyContent:'center',}}> startapp_for_startups.com</Link>
      </Box>
-    <Box sx={{textAlign:'center'}}>
+    <Box sx={{textAlign:'center',fontSize:'20px',color:'black',fontWeight:600}}>
              <div  >99xxxxxxxx</div>
     </Box>
         </Box>
