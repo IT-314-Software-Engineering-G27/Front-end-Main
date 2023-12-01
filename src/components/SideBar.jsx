a975e7f864d8ca9b7b216cb380ca5357d2efd171
sohamviradiya <sohamviradiya2003@gmail.com>
2023-12-01 21:03:56 +0530

    fix option lists

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,9 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React, { useMemo } from "react";
 import { useAuth } from '../contexts/session';
-import { useLocation } from 'react-router-dom';
 import Drawer from "./Drawer";
 import Logo from '../Login/Logo.png';
-import { Link } from "react-router-dom";
 import {
   Event as EventRoundedIcon,
   AccountBoxRounded as AccountBoxRoundedIcon,
@@ -19,64 +17,49 @@ import {
 } from '@mui/icons-material';
 
 const organizationOptionList = [
-  { text: 'Profile', icon: <AccountBoxRoundedIcon />, link: '/profile' },
-  { text: 'Edit Profile', icon: <SettingsAccessibilityRoundedIcon />, link: '/profile/edit' },
   { text: 'Events', icon: <EventRoundedIcon />, link: '/events' },
   { text: 'Job Profile Post', icon: <CloudUploadRoundedIcon />, link: '/organizations/JobRegistration' },
-  { text: 'Contacts', icon: <ContactsRoundedIcon />, link: '/contacts' },
-  { text: 'Posts', icon: <NewspaperRoundedIcon />, link: '/posts' },
-  { text: 'Make Post', icon: <PostAddIcon />, link: '/makepost' },
-  { text: 'Individuals', icon: <PersonRoundedIcon />, link: '/individuals' },
-  { text: 'Organizations', icon: <CorporateFareRoundedIcon />, link: '/organizations' },
 ];
 
 const individualOptionList = [
-  { text: 'Profile', icon: <AccountBoxRoundedIcon />, link: '/profile' },
-  { text: 'Edit Profile', icon: <SettingsAccessibilityRoundedIcon />, link: '/profile/edit' },
   { text: 'Jobs', icon: <WorkRoundedIcon />, link: '/jobs' },
-  { text: 'Contacts', icon: <ContactSupportRoundedIcon />, link: '/contacts' }, 
-  { text: 'Posts', icon: <NewspaperRoundedIcon />, link: '/posts' },
-  { text: 'Make Post', icon: <PostAddIcon />, link: '/makepost' },
-  { text: 'Individuals', icon: <PersonRoundedIcon />, link: '/individuals' },
-  { text: 'Organizations', icon: <CorporateFareRoundedIcon />, link: '/organizations' },
 ];
 
-const commonOptionList = [
+const generalOptionList = [
   { text: 'Home', icon: <img src={Logo} alt="Logo" style={{ width: '24px', height: '24px' }} />, link: '/' },
   { text: 'Contact Us', icon: <ContactSupportRoundedIcon />, link: '/contact-us' },
 ];
 
+const userOptionList = [
+  { text: 'Profile', icon: <AccountBoxRoundedIcon />, link: '/profile' },
+  { text: 'Edit Profile', icon: <SettingsAccessibilityRoundedIcon />, link: '/profile/edit' }, { text: 'Contacts', icon: <ContactsRoundedIcon />, link: '/contacts' },
+  { text: 'Posts', icon: <NewspaperRoundedIcon />, link: '/posts' },
+  { text: 'Make Post', icon: <PostAddIcon />, link: '/makepost' },
+  { text: 'Individuals', icon: <PersonRoundedIcon />, link: '/individuals' },
+  { text: 'Organizations', icon: <CorporateFareRoundedIcon />, link: '/organizations' },
+];
+
+
+
 function SideBar() {
-  // const drawerWidth = 5;
   const auth = useAuth();
-  const location = useLocation();
-  const [role, setRole] = useState("");
+  const optionList = useMemo(() => {
+    if (auth?.session?.user?.individual)
+      return [...generalOptionList, ...userOptionList, ...individualOptionList];
+    else if (auth?.session?.user?.organization)
+      return [...generalOptionList, ...userOptionList, ...organizationOptionList];
+    else
+      return [...generalOptionList];
+  }, [auth?.session?.user?.individual, auth?.session?.user?.organization]);
 
-  useEffect(() => {
-    if (auth?.session?.user?.role) {
-      setRole(auth.session.user.role);
-    }
-  }, [auth?.session?.user]);
-
-  
-  if (role === "organization" || role === "individual") {
-    return (
-      
-        <Drawer optionList={commonOptionList.concat(role === "organization" ? organizationOptionList : individualOptionList)} />
-        
-    
-    );
-  } else {
-    return (
-      <div style={{ display: 'flex', flexDirection: 'row', height: '100%'}}>
-        <div id='drwr'>
-        <Drawer optionList={commonOptionList} />
-        </div>
-        {location.pathname === '/login'}
+  return (
+    <div style={{ display: 'flex', flexDirection: 'row', height: '100%' }}>
+      <div id='drwr'>
+        <Drawer optionList={optionList} />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default SideBar;
 
